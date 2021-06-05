88c44901bbd457af0eb700adfd3c7be1a9c1018b
SriBoom <74517999+SriBoom@users.noreply.github.com>
2021-06-04 18:05:04 -0700

    Add files via upload

--- ReadStoryScreen.js
+++ ReadStoryScreen.js
@@ -1,78 +1,23 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import { Header } from 'react-native-elements';
+import { Text, View, StyleSheet} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default class TransactionScreen extends React.Component {
-    constructor(){
-        super();
-        this.state={
-            title:'',
-            author:'',
-            story:''
-        }
-    }
+export default class WriteStoryScreen extends React.Component{
     render(){
         return(
-          <View style={styles.container}>
-            <Header
-            backgroundColor={'#9c8210'}
-            centerComponent={{
-            text: 'Story Hub',
-            style: { color: '#fff', fontSize: 20 },
-          }}
-            />
-            <TextInput
-            style={styles.inputBox}
-            onChangeText={title => {
-                this.setState({ title: title });
-              }}
-              value={this.state.title}
-            />
-
-            <TextInput
-            style={styles.inputBox}
-            onChangeText={author=>{
-                this.setState({author:author});
-            }}
-            value={this.state.author}
-            />
-
-            <TextInput
-            style={styles.inputBox}
-            multiline
-            onChangeText={story=>{
-                this.setState({story:stroy});
-            }}
-            value={this.state.story}
-            />
-
-            <TouchableOpacity 
-            style={styles.submitButton}
-            >Submit</TouchableOpacity>
+          <SafeAreaProvider>
+            <View style={styles.container}>
+                <Text>Read story</Text>
             </View>
-            
+            </SafeAreaProvider>
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    inputBox: { 
-      width: 200, 
-      height: 40, 
-      borderWidth: 1.5, 
-      borderRightWidth: 0, 
-      fontSize: 20 }, 
-    submitButton:{
-      width: '50%',
-      height: 55,
-      alignSelf: 'center',
-      padding: 10,
-      margin: 10,
-    }
-  });
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+})
--- WriteStoryScreen.js
+++ WriteStoryScreen.js
@@ -1,21 +1,90 @@
 import React from 'react';
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { Header } from 'react-native-elements';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default class WriteStoryScreen extends React.Component{
+export default class TransactionScreen extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            title:'',
+            author:'',
+            story:''
+        }
+    }
     render(){
         return(
-            <View style={styles.container}>
-                <Text>Read story</Text>
+            <SafeAreaProvider>
+          <View style={styles.container}>
+            <Header
+            backgroundColor={'#9c8210'}
+            centerComponent={{
+            text: 'Story Hub',
+            style: { color: '#fff', fontSize: 20, marginLeft:100, textAlign:'center' },
+          }}
+            />
+            <TextInput
+            style={styles.inputBox}
+            onChangeText={title => {
+                this.setState({ title: title });
+              }}
+              value={this.state.title}
+            />
+
+            <TextInput
+            style={styles.inputBox}
+            onChangeText={author=>{
+                this.setState({author:author});
+            }}
+            value={this.state.author}
+            />
+
+            <TextInput
+            style={styles.inputBox}
+            multiline
+            onChangeText={story=>{
+                this.setState({story:stroy});
+            }}
+            value={this.state.story}
+            />
+
+            <TouchableOpacity 
+            style={styles.submitButton}
+            >
+            <Text style={styles.submitButtonText}>Submit</Text>
+            </TouchableOpacity>
             </View>
+            </SafeAreaProvider>
+            
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      },
-})
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    inputBox: { 
+        width: 200,
+        height: 40,
+        borderWidth: 1.5,
+        borderRightWidth: 0,
+        fontSize: 20
+    }, 
+    submitButton:{
+        backgroundColor: '#FBC02D',
+      width: 100,
+      height:50
+    },
+    submitButtonText:{
+        padding: 10,
+      textAlign: 'center',
+      fontSize: 20,
+      fontWeight:"bold",
+      color: 'black'
+    }
+  });
     
